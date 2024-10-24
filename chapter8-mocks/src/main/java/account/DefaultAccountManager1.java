51874c753bdf91eefeecf7395efa7faa6ca19b8f
akm4545 <akm4545@naver.com>
2024-10-24 21:05:51 +0900

    chapter8

---
+++ chapter8-mocks/src/main/java/account/DefaultAccountManager1.java
@@ -0,0 +1,24 @@
+package account;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import java.util.PropertyResourceBundle;
+import java.util.ResourceBundle;
+
+public class DefaultAccountManager1 implements AccountManager{
+    private static final Log logger = LogFactory.getLog(DefaultAccountManager1.class);
+
+    public Account findAccountForUser(String userId){
+        logger.debug("Getting account for user [" + userId + "]");
+        ResourceBundle bundle = PropertyResourceBundle.getBundle("technical");
+        String sql = bundle.getString("FIND_ACCOUNT_FOR_USER");
+
+//        Some code logic to load a user account using JDBC
+        return null;
+    }
+
+    public void updateAccount(Account account){
+        //Perform database access here
+    }
+}
