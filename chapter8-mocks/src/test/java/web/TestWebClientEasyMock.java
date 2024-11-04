d4fd4b17710982ef65e5149c74e6179100613d98
akm4545 <akm4545@naver.com>
2024-11-04 22:12:57 +0900

    chapter8

--- chapter8-mocks/src/test/java/web/TestWebClientEasyMock.java
+++ chapter8-mocks/src/test/java/web/TestWebClientEasyMock.java
@@ -9,6 +9,9 @@ import java.io.InputStream;
 
 import static org.easymock.EasyMock.expect;
 import static org.easymock.EasyMock.expectLastCall;
+//EasyMock의 클래스 확장을 사용하므로 org.easymock.EasyMock이 아니라
+//org.easymock.classextension.EasyMock을 가져온다
+//클래스 확장을 위한 정적 메서드를 사용하여 클래스와 인터페이스의 모의 객체를 만들 수 있다
 import static org.easymock.classextension.EasyMock.createMock;
 import static org.easymock.classextension.EasyMock.replay;
 import static org.easymock.classextension.EasyMock.verify;
@@ -30,6 +33,10 @@ public class TestWebClientEasyMock {
     @Test
     public void testGetContentOk() throws Exception {
         expect(factory.getData()).andReturn(stream);
+//        stream.read() 문장을 실행할 때의 기대를 정의
+//        stream은 마지막에 -1을 읽어 들여야 한다
+//        InputStream 같은 저수준 스트림은 바이트 단위로 데이터를 읽어 오므로 한 번에
+//        한 바이트씩 읽는 방법을 정의한다
         expect(stream.read()).andReturn(Integer.valueOf((byte)'W'));
         expect(stream.read()).andReturn(Integer.valueOf((byte)'o'));
         expect(stream.read()).andReturn(Integer.valueOf((byte)'r'));
@@ -38,8 +45,12 @@ public class TestWebClientEasyMock {
         expect(stream.read()).andReturn(Integer.valueOf((byte)'!'));
 
         expect(stream.read()).andReturn(-1);
+        
+//        stream.close 메서드가 호출될 것을 기대한다
         stream.close();
 
+//        replay 메서드를 호출하기 전에는 모의 객체가 수행해야 하는 작업을 기록만 할 뿐
+//        모의 객체를 호출하더도 동작을 수행하지 않는다
         replay(factory);
         replay(stream);
 
@@ -68,7 +79,9 @@ public class TestWebClientEasyMock {
     public void testGetContentCannotCloseInputStream() throws Exception{
         expect(factory.getData()).andReturn(stream);
         expect(stream.read()).andReturn(-1);
+//        stream을 닫을 수 없을 때의 조건을 모사
         stream.close();
+//        호출이 발생했을 경우 IOException이 발생해야 한다고 선언
         expectLastCall().andThrow(new IOException("cannot close"));
 
         replay(factory);
