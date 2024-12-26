9a1dd2c3654595fdcc4e3c46cbadb3f32322b65d
akm4545 <akm4545@naver.com>
2024-12-26 23:08:45 +0900

    chapter19

---
+++ chapter19-databases/src/test/java/hibernate/CountriesHibernateTest.java
@@ -0,0 +1,93 @@
+package hibernate;
+
+import hibernate.model.Country;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class CountriesHibernateTest {
+
+//    엔티티 매니저를 생성하기 위한 팩토리
+    private EntityManagerFactory emf;
+//    엔티티를 통해 DB에 접근할 수 있도록 하는 엔티티 매니저
+    private EntityManager em;
+
+    private List<Country> expectedCountryList = new ArrayList<>();
+    private List<Country> expectedCountryListStartsWithA = new ArrayList<>();
+
+    public static final String[][] COUNTRY_INIT_DATA = {{"Australia", "AU"}, {"Canada", "CA"}, {"France", "FR"},
+            {"Germany", "DE"}, {"Italy", "IT"}, {"Japan", "JP"}, {"Romania", "RO"},
+            {"Russian Federation", "RU"}, {"Spain", "ES"}, {"Switzerland", "CH"},
+            {"United Kingdom", "UK"}, {"United States", "US"}};
+
+    @BeforeEach
+    public void setUp() {
+        initExpectedCountryLists();
+
+        emf = Persistence.createEntityManagerFactory("manning.hibernate");
+        em = emf.createEntityManager();
+
+//        트랜잭션 내에서 국가 객체를 데이터베이스에 영속
+        em.getTransaction().begin();
+
+        for(int i=0; i<COUNTRY_INIT_DATA.length; i++){
+            String[] countryInitData = COUNTRY_INIT_DATA[i];
+            Country country = new Country(countryInitData[0], countryInitData[1]);
+            em.persist(country);
+        }
+
+        em.getTransaction().commit();
+    }
+
+    @Test
+    public void testCountryList() {
+        List<Country> countryList = em.createQuery("select c from Country c").getResultList();
+
+        assertNotNull(countryList);
+        assertEquals(COUNTRY_INIT_DATA.length, countryList.size());
+
+        for(int i=0; i<expectedCountryList.size(); i++){
+            assertEquals(expectedCountryList.get(i), countryList.get(i));
+        }
+    }
+
+    @Test
+    public void testCountryListStartsWithA() {
+        List<Country> countryList = em.createQuery("select c from Country c where c.name like 'A%'").getResultList();
+
+        assertNotNull(countryList);
+        assertEquals(expectedCountryListStartsWithA.size(), countryList.size());
+
+        for(int i=0; i<expectedCountryListStartsWithA.size(); i++){
+            assertEquals(expectedCountryListStartsWithA.get(i), countryList.get(i));
+        }
+    }
+
+    @AfterEach
+    public void dropDown() {
+        em.close();
+        emf.close();
+    }
+
+    private void initExpectedCountryLists() {
+        for(int i=0; i<COUNTRY_INIT_DATA.length; i++){
+            String[] countryInitData = COUNTRY_INIT_DATA[i];
+            Country country = new Country(countryInitData[0], countryInitData[1]);
+            expectedCountryList.add(country);
+
+            if(country.getName().startsWith("A")){
+                expectedCountryListStartsWithA.add(country);
+            }
+        }
+    }
+}
