8717164e470098ccd384a4c8234802fd1f02a615
akm4545 <akm4545@naver.com>
2024-12-29 20:30:45 +0900

    chapter20

---
+++ chapter20-tdd/src/main/java/before/Airport.java
@@ -0,0 +1,28 @@
+package before;
+
+public class Airport {
+
+    public static void main(String[] args){
+        Flight economyFlight = new Flight("1", "Economy");
+        Flight businessFlight = new Flight("2", "Business");
+
+        Passenger james = new Passenger("James", true);
+        Passenger mike = new Passenger("Mike", false);
+
+        businessFlight.addPassenger(james);
+        businessFlight.removePassenger(james);
+        businessFlight.addPassenger(mike);
+
+        economyFlight.addPassenger(mike);
+
+        System.out.println("비즈니스 항공편 승객 리스트:");
+        for (Passenger passenger : businessFlight.getPassengersList()){
+            System.out.println(passenger.getName());
+        }
+
+        System.out.println("이코노미 항공편 승객 리스트:");
+        for(Passenger passenger : economyFlight.getPassengersList()){
+            System.out.println(passenger.getName());
+        }
+    }
+}
