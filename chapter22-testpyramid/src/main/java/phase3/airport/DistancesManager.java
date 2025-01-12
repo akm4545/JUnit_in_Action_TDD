785da9c8d723276a65a4837581054280bb03114a
akm4545 <akm4545@naver.com>
2025-01-12 19:07:13 +0900

    chapter22

--- chapter22-testpyramid/src/main/java/phase3/airport/DistancesManager.java
+++ chapter22-testpyramid/src/main/java/phase3/airport/DistancesManager.java
@@ -1,15 +1,24 @@
 package phase3.airport;
 
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
 public class DistancesManager {
 
+    private static final int DISTANCE_FACTOR = 10;
+
+    private Map<Passenger, Integer> passengersDistancesMap = new HashMap<>();
+    private Map<Passenger, Integer> passengersPointsMap = new HashMap<>();
+
+
     public Map<Passenger, Integer> getPassengersDistancesMap() {
-        return null;
+        return Collections.unmodifiableMap(passengersDistancesMap);
     }
 
     public Map<Passenger, Integer> getPassengersPointsMap() {
-        return null;
+        return Collections.unmodifiableMap(passengersPointsMap);
+
     }
 
     public void addDistance(Passenger passenger, int distance){
@@ -17,6 +26,8 @@ public class DistancesManager {
     }
 
     public void calculateGivenPoints() {
-
+        for(Passenger passenger : getPassengersDistancesMap().keySet()){
+            passengersPointsMap.put(passenger, getPassengersDistancesMap().get(passenger) / DISTANCE_FACTOR);
+        }
     }
 }
