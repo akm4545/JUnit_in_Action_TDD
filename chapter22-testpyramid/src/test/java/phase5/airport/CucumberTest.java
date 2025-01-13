a12be46ecf41016e552b1a6cd571b6a46e9271ed
akm4545 <akm4545@naver.com>
2025-01-13 22:01:10 +0900

    chapter22

---
+++ chapter22-testpyramid/src/test/java/phase5/airport/CucumberTest.java
@@ -0,0 +1,12 @@
+package phase5.airport;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"pretty"},
+        features = "classpath:features")
+public class CucumberTest {
+}
